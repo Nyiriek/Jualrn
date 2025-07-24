788d2c6841c6777aca148c3f0bd06421d332500d
Nyiriek <n.peat@alustudent.com>
2025-07-24 20:55:04 +0200

    Add changes

--- JuaLearn/jualearn_backend_root/jualearnbackend/wsgi.py
+++ JuaLearn/jualearn_backend_root/jualearnbackend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jualearn_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jualearnbackend.settings')
 
 application = get_wsgi_application()
