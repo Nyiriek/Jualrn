cf7cd4363608e852612338dd6ca344785af7bf73
Nyiriek <n.peat@alustudent.com>
2025-07-25 17:24:10 +0200

    changes

--- JuaLearn/jualearn_backend_root/jualearnbackend/urls.py
+++ JuaLearn/jualearn_backend_root/jualearnbackend/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 from users.views import MyTokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
 
+def home(request):
+    return HttpResponse("Welcome to JuaLearn API")
+
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
-    # JWT endpoints (standard)
     path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
